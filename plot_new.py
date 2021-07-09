30e3f72a13d47e7012122a8c899955bc04407548
梁哲榕 <zrliang1205@gmail.com>
2021-07-09 18:42:35 +0800

    first version of output

---
+++ plot_new.py
@@ -0,0 +1,50 @@
+import plotly.express as px
+import pandas as pd
+import datetime
+import time # 引入time
+
+# fixme
+result = pd.read_csv("result(0707_2).csv")
+#df2 = pd.read_csv('WipOutPlanTime_.csv')
+#result = pd.merge(df1, df2, on ='wlot_lot_number', how ='left')
+entities= result.groupby("entity") # group by entity
+# entities.groups #show dict, value is row index
+entity_key_list= list(entities.groups.keys()) # key list
+
+# 2021/4/17 08:30 (改 變數)
+ini_time_stamp = 1618619400 
+
+df=[]
+count=0
+for i in range(len(entity_key_list)): 
+    key= entity_key_list[i]
+    count+=1
+    for j in range(len(entities.groups[key])):
+        row_index=entities.groups[key][j]
+
+        Lot_id =result.iloc[row_index].at['lot_number'].strip()
+        Entity =result.iloc[row_index].at['entity'].strip()
+        start_ts =  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ini_time_stamp+result.iloc[row_index].at['start_time']*60)) 
+        end_ts =  time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(ini_time_stamp+result.iloc[row_index].at['end_time']*60)) 
+        Sets = result.iloc[row_index].at['part_no'].strip()+ "/"+ result.iloc[row_index].at['part_id'].strip()
+        Bd_id= result.iloc[row_index].at['bd_id'].strip()
+    
+
+        df.append(dict(Lot_ID= Lot_id, Start=start_ts,Finish=end_ts,Entity=Entity,Sets=Sets,Bd_Id=Bd_id))
+    if(count==50) or i==len(entity_key_list)-1: #變數
+       #呈現圖表
+        fig = px.timeline(df, x_start="Start", x_end="Finish", y="Entity", color="Bd_Id",text="Lot_ID",hover_name="Sets")   # hover_name
+        fig.update_yaxes(categoryorder="category descending") # 
+        fig.update_traces(textposition='inside',marker_line_color='rgb(8,48,107)')
+        fig.write_html("C:/Users/LiangCJ/Desktop/plot_result/"+df[0]['Entity']+"_" +df[-1]['Entity']+".html")
+        df=[]
+        count=0
+ 
+
+
+
+# #呈現圖表
+# fig1 = px.timeline(df, x_start="Start", x_end="Finish", y="Entity", color="Lot_ID",text="Lot_ID")
+# fig1.update_yaxes(categoryorder="category descending") # 
+# fig1.show()
+# fig1.show()
